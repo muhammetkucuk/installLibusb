65ffdbacb75a6ea646ca85d1e7e1da77689ecd58
Muhammet Küçük <iletisim@muhammetkucuk.com>
2018-08-29 10:19:52 +0300

    script update

--- installLibusb.sh
+++ installLibusb.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+sudo apt-get update -y &&
+sudo apt-get upgrade -y &&
+sudo apt-get dist-upgrade -y &&
+sudo apt-get autoremove -y &&
+sudo apt-get install build-essential -y &&
 wget https://github.com//libusb/libusb/releases/download/v1.0.22/libusb-1.0.22.tar.bz2 &&
 tar jxf libusb-1.0.22.tar.bz2 &&
 cd libusb-1.0.22 &&
@@ -10,4 +15,5 @@ sudo make install &&
 cd .. &&
 rm -R libusb-1.0.22 &&
 rm libusb-1.0.22.tar.bz2 &&
+sudo ldconfig -v &&
 sudo ldconfig
